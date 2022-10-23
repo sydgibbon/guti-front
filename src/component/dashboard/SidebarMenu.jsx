e8e2279a36d5b11b04f921a6d22dfc4b8a3d0090
Sydney Gibbon <sydney@leadboxhq.com>
2022-10-23 20:59:09 -0300

    Created atomic folder and Item.jsx file

--- src/component/dashboard/SidebarMenu.jsx
+++ src/component/dashboard/SidebarMenu.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BsChevronCompactDown } from "react-icons/bs";
+import Item from "../atomic/Item";
 
 const SidebarMenu = ({ text, icon, subMenu }) => {
   const [selected, setSelected] = useState("");
@@ -10,10 +11,7 @@ const SidebarMenu = ({ text, icon, subMenu }) => {
         className={` py-2 px-3 flex justify-between hover:border-l-2 border-primary-light cursor-normal select-none ${selected && "shadow-md border-l-2"} `}
         onClick={toggle}
       >
-        <div className="flex text-ellipsis">
-          {icon}
-          <p className="pl-2">{text}</p>
-        </div>
+        <Item icon={ icon } text={ text }/>
         <BsChevronCompactDown className="block my-auto" />
       </div>
       {selected && subMenu}
--- src/component/dashboard/SidebarSubMenu.jsx
+++ src/component/dashboard/SidebarSubMenu.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import Item from "../atomic/Item";
 
 const SidebarSubMenu = ({text, icon}) => {
   return (
     <div className="py-2 pl-6 flex justify-between hover:border-l-2 border-primary-light cursor-normal select-none">
-      <div className="flex text-ellipsis">
-        {icon}
-        <p className="pl-2">{text}</p>
-      </div>
+      <Item icon={ icon } text={ text }/>
     </div>
   )
 }
