0824b44fa08e6e9f80fe5cf029f7b70fed263bfa
GusChoque <110579994+GusChoque@users.noreply.github.com>
2022-10-27 15:50:57 -0300

    added inputs in netdevices form

--- src/component/forms/assets/NetworkDevicesForm.jsx
+++ src/component/forms/assets/NetworkDevicesForm.jsx
@@ -10,19 +10,68 @@ const NetworkDevicesForm = () => {
                     <p className='text-base my-auto font-medium'>New Item - Network Device</p>
                 </div>
                 <div className='form-container h-full w-full p-4 border border-secondary-dark bg-white flex justify-between'>
-                <div className='first-form w-1/2 mx-4'>
+                    <div className='first-form w-1/2 mx-4'>
+                        <div className='input-name mb-4'>
+                            <p className='text-sm mb-2'>Name</p>
+                            <input className='w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray'></input>
+                        </div>
+                        <div className='input-locations my-4'>
+							<p className='text-sm mb-2'>Locations</p>
+							<div className='flex divide-x divide-x-reverse'>
+								<select className='w-full px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray'>
+									<option value="-----">-----</option>
+								</select>
+								<div className='h-9 border border-secondary-dark flex items-center'>
+									<TbList className='mx-2' />
+								</div>
+								<div className='h-9 border border-secondary-dark flex items-center'>
+									<TbPlus className='mx-2' />
+								</div>
+								<div className='rounded-r-md h-9 border border-secondary-dark flex items-center'>
+									<TbMap className='mx-2' />
+								</div>
+							</div>
+						</div>
+                        <div className='input-technitian my-4'>
+							<p className='text-sm mb-2'>Technician in Charge of the Hardware</p>
+							<div className='flex divide-x divide-x-reverse'>
+								<select className='w-full px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray'>
+									<option value="-----">-----</option>
+								</select>
+								<div className='h-9 border rounded-r-md border-secondary-dark flex items-center'>
+									<TbList className='mx-2' />
+								</div>
+							</div>
+						</div>
+                        <div className='input-group-in-charge my-4'>
+							<p className='text-sm mb-2'>Group in Charge of the Hardware</p>
+							<div className='flex divide-x divide-x-reverse'>
+								<select className='w-full px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray'>
+									<option value="-----">-----</option>
+								</select>
+								<div className='h-9 border border-secondary-dark flex items-center'>
+									<TbList className='mx-2' />
+								</div>
+								<div className='h-9 border rounded-r-md border-secondary-dark flex items-center'>
+									<TbPlus className='mx-2' />
+								</div>
+							</div>
+						</div>
+                        <div className='input-name my-4'>
+							<p className='text-sm mb-2'>Name</p>
+							<input className='w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray'></input>
+						</div>
+                    </div>
+                    <div className='second-form w-1/2 mx-4'>
 
-                </div>
-                <div className='second-form w-1/2 mx-4'>
-                    
-                </div>
+                    </div>
                 </div>
                 <div className='form-footer flex h-1/6 w-full border rounded-b-md border-secondary-dark justify-end mb-2 bg-white py-4 px-5'>
-					<button className='flex items-center border-2 border-orange-dark rounded-md bg-primary-light text-white hover:border-black py-2 px-4'>
-						<TbPlus className='mr-1' size="20" />
-						<p className='text-lg font-medium'>Add</p>
-					</button>
-				</div>
+                    <button className='flex items-center border-2 border-orange-dark rounded-md bg-primary-light text-white hover:border-black py-2 px-4'>
+                        <TbPlus className='mr-1' size="20" />
+                        <p className='text-lg font-medium'>Add</p>
+                    </button>
+                </div>
             </form>
         </div>
     )
