71453a117467845ace6cf9602416de5b0d0dc591
Nataniel <nataniel.nzh.2018@gmail.com>
2022-10-30 16:36:50 -0300

    Created PhonesForm

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AssetsDashboard from "./component/dashboard/AssetsDashboard";
 import axiosPrivate from "./api/axios";
 import Computers from "./pages/Computers";
 import ComputerForm from "./component/forms/assets/ComputerForm";
+import PhonesForm from "./component/forms/assets/PhonesForm";
 
 
 
@@ -69,6 +70,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/assets" element={<Main content={ <AssetsDashboard />} />} />
         <Route path="/computer-form" element={<Main content={ <ComputerForm />} />} />
+        <Route path="/phones-form" element={<Main content={ <PhonesForm />} />} />
         <Route path="/computers" element={<Main content={ <Computers />} />} />
       </Routes>
       <button
