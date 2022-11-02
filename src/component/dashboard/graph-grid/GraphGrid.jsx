253ca148a0f37119fca798049ef6026813587068
Nataniel <nataniel.nzh.2018@gmail.com>
2022-11-02 14:26:47 -0300

    Updated Donut graph

--- src/component/dashboard/graph-grid/GraphGrid.jsx
+++ src/component/dashboard/graph-grid/GraphGrid.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const GraphGrid = ({ icon, graph, text, bgColor, textColor }) => {
   return (
-    <div className={`graph-grid-container grid grid-cols-1 select-none w-36 h-50 border rounded-md  drop-shadow-lg p-1 ${bgColor}`} >
+    <div className={`graph-grid-container grid grid-cols-1 select-none w-36 h-60 border rounded-md  drop-shadow-lg p-1 ${bgColor}`} >
       <div className=" ">
         {graph}
       </div>
-      <div className="flex items-center">
+      <div className={`flex items-center border-t b-[${textColor}]`}>
         <span className={`text-sm font-bold ${textColor}`}>{text}</span>
         <span className={` ${textColor}`}>{icon}</span>
       </div>
