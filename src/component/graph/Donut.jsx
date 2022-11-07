73f5ff8dd7d3f74747e3529aa1d911d7d5d9ed90
Sydney Gibbon <sydney@leadboxhq.com>
2022-11-06 23:28:32 -0300

    Finished Donut Graph

--- src/component/graph/Donut.jsx
+++ src/component/graph/Donut.jsx
@@ -1,12 +1,9 @@
-import { React, useEffect, useState } from 'react';
+import { React, useEffect, useState } from "react";
 import axiosPrivate from "../../api/axios";
-import { Doughnut } from 'react-chartjs-2';
-import { Chart, Tooltip, Title, ArcElement, Legend } from 'chart.js';
+import { Doughnut } from "react-chartjs-2";
+import { Chart, Tooltip, Title, ArcElement, Legend } from "chart.js";
 
-Chart.register(
-    Tooltip, Title, ArcElement, Legend
-);
-
+Chart.register(Tooltip, Title, ArcElement, Legend);
 
 const Donut = ({ asset }) => {
   const [checked, setChecked] = useState(false);
@@ -16,7 +13,7 @@ const Donut = ({ asset }) => {
   useEffect(() => {
     async function getData() {
       await axiosPrivate
-        .get("http://127.0.0.1:8000/api/assets/"+asset+"/")
+        .get("http://127.0.0.1:8000/api/assets/" + asset + "/")
         .then((response) => {
           // check if the data is populated
           console.log(response.data);
@@ -31,37 +28,52 @@ const Donut = ({ asset }) => {
     }
   }, []);
 
+  const getComputersPerType = (data) => {
+    let repeatedTypes = [];
+
+    data.map((obj) => {
+      repeatedTypes.push(obj.computertypes_id);
+    });
+    const objTypeCounter = {};
+
+    repeatedTypes.forEach((element) => {
+      objTypeCounter[element] = (objTypeCounter[element] || 0) + 1;
+    });
+    return objTypeCounter;
+  };
+  let keys = Object.keys(getComputersPerType(data));
+
+  let values = Object.values(getComputersPerType(data));
+
+
   const options = {
-    plugins:{
-     legend: {
-      display: false
-     },
-     weight: 3
-    }
-   }
+    plugins: {
+      legend: {
+        display: false,
+      },
+      weight: 3,
+    },
+  };
 
   const dataSet = {
-    labels: [
-      'Laptop',
-      'Allinone',
-      'Desk'
-    ],
-    datasets: [{
-        data: [ 300, 50, 15 ],
+    labels: keys,
+    datasets: [
+      {
+        data: values,
         backgroundColor: [
-            'rgb(255, 99, 132)',
-            'rgb(54, 162, 235)',
-            'rgb(255, 205, 86)'
-          ],
-    }],
-};
+          "rgb(255, 99, 132)",
+          "rgb(54, 162, 235)",
+          "rgb(255, 205, 86)",
+        ],
+      },
+    ],
+  };
 
   return (
-    <div> 
-        <Doughnut className='' data={dataSet} options={options} />
-    </div>
-  )
-}
+    <>
+      <Doughnut className="" data={dataSet} options={options} />
+    </>
+  );
+};
 
-export default Donut
-
+export default Donut;
