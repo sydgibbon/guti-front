bb3fd156197343dd67f723e2b031783c67204963
Nataniel <nataniel.nzh.2018@gmail.com>
2022-11-09 19:16:09 -0300

    Updated CableForm

--- src/component/forms/assets/CableForm.jsx
+++ src/component/forms/assets/CableForm.jsx
@@ -7,14 +7,14 @@ const CableForm = () => {
     <div className="computer-form flex flex-col items-center divide-y divide-y-reverse m-4">
       {/* <div> de Form Header */}
       {/* usé el "w-[calc(100%-1rem)]" para que quede separado en el eje X ayq eu con el mx no se podia darle margen */}
-      <div className="form-header flex bg-medium-gray h-1/6 w-full border rounded-t-md border-secondary-dark mt-2 mx-auto py-4 px-5">
+      <div className="form-header flex bg-medium-gray h-1/6 w-full  mt-2 mx-auto py-4 px-5">
         <TbColumns className="inline my-auto mx-2 w-6 h-5" />
         <p className="text-base my-auto font-medium">New Item - Enclosures</p>
       </div>
 
       {/* <div> contenedor del Form */}
-      <div className=" form-container h-full w-full p-4 border border-secondary-dark bg-white">
-        <form className="grid grid-cols-2 gap-4 ">
+      <div className=" form-container h-full w-full p-4 bg-white ">
+        <form className="grid grid-cols-2 gap-4">
           <div className="input-name">
             <p className="text-sm mb-2">Name</p>
             <input className="w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray"></input>
@@ -98,21 +98,143 @@ const CableForm = () => {
               className="w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray"
             ></input>
           </div>
-
-
-          <div className="w-full flex h-8 p-5 border-y-1 items-center bg-[#FAFBFE] ">
-            <span className="w-">Endpoint A</span>
-          </div>
-
-          <div className="w-full flex h-8 p-5 border-y-1 items-center bg-[#FAFBFE] ">
-            <span className="w-">Endpoint A</span>
-          </div>
-
         </form>
       </div>
 
+      {/* <div> contenedor del Endpoint */}
+      <div className="endpoint-form-container grid grid-cols-2 w-full bg-white ">
+        <div className="endpoint-content ">
+          <div className="card-header py-4 px-5 ml-2 bg-secondary-dark  border-black rounded-tl">
+            <h4 className="font-medium">Endpoint A</h4>
+          </div>
+
+          <form className="card-body p-4 px-5">
+            <div className="input-asset ">
+              <p className="text-sm mb-2">Asset</p>
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-full px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="computers">Computers</option>
+                  <option value="network-devices">Network devices</option>
+                  <option value="devices">Devices</option>
+                  <option value="phones">Phones</option>
+                  <option value="printers">Printers</option>
+                  <option value="pasive-devices">Pasive devices</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="input ">
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-1/4 px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="-----">-----</option>
+                </select>
+                <div className="cursor-pointer h-9 border rounded-r-md border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbList className="mx-2 " />
+                </div>
+              </div>
+            </div>
+
+            <div className="input-socket-model ">
+              <p className="text-sm mb-2">Socket model</p>
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-1/4 px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="-----">-----</option>
+                </select>
+                <div className="cursor-pointer h-9 border border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbList className="mx-2 " />
+                </div>
+                <div className="cursor-pointer h-9 border rounded-r-md border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbPlus className="mx-2 " />
+                </div>
+              </div>
+            </div>
+
+            <div className="input-socket ">
+              <p className="text-sm mb-2">Socket</p>
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-1/4 px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="-----">-----</option>
+                </select>
+                <div className="cursor-pointer h-9 border border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbList className="mx-2 " />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label>Position</label>
+            </div>
+
+
+          </form>
+        </div>
+
+        <div className="endpoint-content">
+          <div className="card-header p-4 px-5 mr-3 bg-secondary-dark border-black rounded-tr-lg">
+            <h4 className="font-medium">Endpoint B</h4>
+          </div>
+          <form className="card-body p-4 px-5">
+          <div className="input-asset ">
+              <p className="text-sm mb-2">Asset</p>
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-full px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="computers">Computers</option>
+                  <option value="network-devices">Network devices</option>
+                  <option value="devices">Devices</option>
+                  <option value="phones">Phones</option>
+                  <option value="printers">Printers</option>
+                  <option value="pasive-devices">Pasive devices</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="input ">
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-1/4 px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="-----">-----</option>
+                </select>
+                <div className="cursor-pointer h-9 border rounded-r-md border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbList className="mx-2 " />
+                </div>
+              </div>
+            </div>
+
+            <div className="input-socket-model ">
+              <p className="text-sm mb-2">Socket model</p>
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-1/4 px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="-----">-----</option>
+                </select>
+                <div className="cursor-pointer h-9 border border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbList className="mx-2 " />
+                </div>
+                <div className="cursor-pointer h-9 border rounded-r-md border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbPlus className="mx-2 " />
+                </div>
+              </div>
+            </div>
+
+            <div className="input-socket ">
+              <p className="text-sm mb-2">Socket</p>
+              <div className="flex divide-x divide-x-reverse ">
+                <select className="cursor-pointer w-1/4 px-2 rounded-l-md h-9 border border-secondary-dark bg-medium-gray">
+                  <option value="-----">-----</option>
+                </select>
+                <div className="cursor-pointer h-9 border border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
+                  <TbList className="mx-2 " />
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <label>Position</label>
+            </div>
+          </form>
+        </div>
+      </div>
+
       {/* <div> del footer */}
-      <div className="form-footer flex h-1/6 w-full border rounded-b-md border-secondary-dark justify-end mb-2 bg-white py-4 px-5">
+      <div className="form-footer flex h-1/6 w-full  justify-end mb-2 bg-white py-4 px-5">
         <button className="flex items-center mr-4 border-2 border-orange-dark rounded-md bg-primary-light text-white hover:border-black py-2 px-4">
           <TbPlus className="mr-1" />
           <p className="text-lg font-medium">Add</p>
