544581e5f4acfbda6f9622cc040d67545c02cab1
GusChoque <110579994+GusChoque@users.noreply.github.com>
2022-11-12 02:03:40 -0300

    cartridges and consumables

--- src/pages/Cartridges.jsx
+++ src/pages/Cartridges.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+import React from 'react';
 import SearchCard from '../component/dashboard/assets/SearchCard';
 import SearchForm from '../component/dashboard/SearchForm';
 
 const Cartridges = () => {
-    const itemProps = [
-        "name",
-        "ref",
-        "cartridgeitemtypes_id",
-        "manufacturers_id",
-        "locations_id",
-        "id", // Total	1	New	1 Used	0	Worn	0
-      ];
-      const columns = [
-        "NAME",
-        "REFERENCE",
-        "TYPES",
-        "MANUFACTURERS",
-        "LOCATIONS",
-        "CARTRIDGES"
-      ];
-      return (
-        <div className='cartridges'>
-          <SearchForm />
-          <SearchCard asset="cartridgeitems" columns={columns} itemProps={itemProps}/>
-        </div>
-      )
+  const columns = [
+    {
+      name: "NAME",
+      selector: (row) => row.name,
+    },
+    {
+      name: "REFERENCE",
+      selector: (row) => row.ref,
+    },
+    {
+      name: "TYPE",
+      selector: (row) => row.cartridgeitemtypes_id[0]["name"],
+    },
+    {
+      name: "MANUFACTURERS",
+      selector: (row) => row.manufacturers_id[0]["name"],
+    },
+    {
+      name: "LOCATIONS",
+      selector: (row) => row.locations_id[0]["name"],
+    },
+    {
+      name: "CARTRIDGES",
+      selector: (row) => row.id,
+    }, // revisar
+  ];
+
+  return (
+    <div className="cartridges">
+      <SearchForm />
+      <SearchCard asset="cartridgeitems" columns={columns} />
+    </div>
+  )
 }
 
 export default Cartridges
