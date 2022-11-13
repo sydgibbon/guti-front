2d3a63bb77f3ce3940563679ab79f803dedbd7dd
Nataniel <nataniel.nzh.2018@gmail.com>
2022-11-13 16:46:34 -0300

    Updated Devices and Printers

--- src/pages/Devices.jsx
+++ src/pages/Devices.jsx
@@ -2,30 +2,44 @@ import React from 'react';
 import SearchCard from '../component/dashboard/assets/SearchCard';
 import SearchForm from '../component/dashboard/SearchForm';
 const Devices = () => {
-    const itemProps = [
-        "name",
-        "states_id",
-        "manufacturers_id",
-        "locations_id",
-        "peripheraltypes_id",
-        "peripheralmodels_id",
-        "date_mod",
-        "contact"
-      ];
       const columns = [
-        "NAME",
-        "STATUS",
-        "MANUFACTURERS",
-        "LOCATIONS",
-        "TYPES",
-        "MODEL",
-        "LAST UPDATE",
-        "ALTERNATE USERNAME",
+        {
+          name: "NAME",
+          selector: (row) => row.name,
+        },
+        {
+          name: "STATUS",
+          selector: (row) => row.states_id[0]["name"], 
+        },
+        {
+          name: "MANUFACTURERS",
+          selector: (row) => row.manufacturers_id[0]["name"],
+        },
+        {
+          name: "LOCATIONS",
+          selector: (row) => row.locations_id[0]["name"], 
+        },
+        {
+          name: "TYPES",
+          selector: (row) => row.peripheraltypes_id[0]["name"], 
+        },
+        {
+          name: "MODEL",
+          selector: (row) => row.peripheralmodels_id[0]["name"]
+        },
+        {
+          name: "LAST UPDATE",
+          selector: (row) => row.date_mod, 
+        },
+        {
+          name: "ALTERNATE USERNAME",
+          selector: (row) => row.contact, 
+        },
       ];
       return (
         <div className='devices'>
           <SearchForm />
-          <SearchCard asset="peripherals" columns={columns} itemProps={itemProps}/>
+          <SearchCard asset="peripherals" columns={columns} />
         </div>
       )
 }
