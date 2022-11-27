b3a6c49fd7fc9cedc4dbf6cb460e4aa53a0e269d
GusChoque <110579994+GusChoque@users.noreply.github.com>
2022-11-27 20:01:24 -0300

    completed locations subform

--- src/component/subforms/LocationsSubForm.jsx
+++ src/component/subforms/LocationsSubForm.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { TbPlus, TbList, TbMap } from "react-icons/tb";
+import { TbPlus, TbList, TbMap, TbCircleX } from "react-icons/tb";
+import { BsLaptop } from "react-icons/bs";
 
 function LocationsSubForm(props) {
   return (props.trigger) ? (
-    <div className='absolute w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 m-4 flex justify-center items-center rounded-md border border-secondary-dark'>
-      <div className='relative p-12 w-full h-full bg-slate-100'>
-        <form action="">
-          <div className='grid grid-cols-2 gap-x-8'>
+    <div className='absolute w-1/2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+      <div className='relative w-full h-full bg-white p-4 divide-y border rounded-t-md border-secondary-dark '>
+        {/* <button className='top-4 right-4' onClick={() => props.setTrigger(false)}>Cerrar</button> */}
+        <div className='flex justify-end items-center mb-3'>
+          <TbCircleX className='' onClick={() => props.setTrigger(false)} size="24"></TbCircleX>
+        </div>
+        <div className="form-header flex justify-center md:justify-start bg-medium-gray h-1/6 w-full border rounded-t-md border-secondary-dark py-4 px-5">
+          <BsLaptop className="inline my-auto mr-2" size="24" />
+          <p className="text-base my-auto font-medium">New Items - Locations</p>
+        </div>
+        <form action="" className='divide-y divide-y-reverse'>
+          <div className='grid grid-cols-2 gap-x-8 border border-secondary-dark bg-white md:p-4'>
             <div className="input-name">
               <p className="text-sm mb-2">Name</p>
               <input className="w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray"></input>
@@ -62,7 +71,7 @@ function LocationsSubForm(props) {
             </div>
             <div className="input-location-map">
               <p className="text-sm mb-2">Location on map</p>
-              
+
               <input className="w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray" placeholder='Aca va un MAPA'></input>
             </div>
             <div className="input-latitude">
@@ -78,8 +87,14 @@ function LocationsSubForm(props) {
               <input className="w-full px-2 rounded-md h-9 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
           </div>
+          <div className="form-footer flex h-1/6 w-full border rounded-b-md border-secondary-dark justify-center md:justify-end mb-2 bg-white py-4 px-5">
+            <button className="flex items-center md:mr-4 border-2 border-orange-dark rounded-md bg-primary-light text-white hover:border-black py-2 px-4">
+              <TbPlus className="mr-1" />
+              <p className="text-lg font-medium">Add</p>
+            </button>
+          </div>
         </form>
-        <button className='top-4 right-4' onClick={() => props.setTrigger(false)}>Cerrar</button>
+
         {props.children}
       </div>
     </div>
