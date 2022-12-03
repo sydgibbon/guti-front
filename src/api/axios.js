8a26f7abac559f657bab80cb0eab143e50566443
FranchJavier <franciscojavierlieg@gmail.com>
2022-12-02 22:02:21 -0300

    Revert "favicon added"
    This reverts commit ccc3b5c80606ff62c0d45c8028c1049456c8edb1.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const BASE_URL = 'http://127.0.0.1:8000/api/assets/';
-const TOKEN = 'b7ef9e2b5b33777aba819fbe2d375bfa93db5923';
+const TOKEN = 'fe2cd32a6c7b8f8d28a505ef574c971ae8045a7f';
 
 export default axios.create({
     baseURL: BASE_URL,
