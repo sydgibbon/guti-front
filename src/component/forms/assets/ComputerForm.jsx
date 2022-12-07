ed5e157d2f6c03745b19dddd2852a12d22dab56d
GusChoque <110579994+GusChoque@users.noreply.github.com>
2022-12-07 13:10:05 -0300

    added more changes to modal

--- src/component/forms/assets/ComputerForm.jsx
+++ src/component/forms/assets/ComputerForm.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { BsLaptop } from "react-icons/bs";
 import { TbPlus, TbList, TbMap } from "react-icons/tb";
-import LocationsSubForm from "../../subforms/LocationsSubForm";
 import SelectComponent from "../../atomic/SelectComponent";
-import {saveAsset, BASE_URL} from "../../../api/axios";
+import { saveAsset, BASE_URL } from "../../../api/axios";
 import LocationsSubFormFE99 from "../../subforms/LocationsSubForm-FE99";
 
 const ComputerForm = () => {
@@ -22,18 +21,16 @@ const ComputerForm = () => {
   const handleChangeSelect = (data) => {
     formFields[data.id] = data.value;
   }
-    const [ButtonPopup, setButtonPopup] = useState(false);
 
 
-  
-    return (
-      // <div> general
+
+  return (
+    // <div> general
     <div className="computer-form flex flex-col items-center m-4">
- 
-      
-    <form onSubmit={handleSubmit} action={`${BASE_URL}computers/`} method="POST" encType="multipart/form-date" className="w-full divide-y divide-y-reverse">
-        {/* <div> de Form Header */}
-        <LocationsSubForm trigger={ButtonPopup} setTrigger={setButtonPopup}></LocationsSubForm>
+
+
+      <form onSubmit={handleSubmit} action={`${BASE_URL}computers/`} method="POST" encType="multipart/form-date" className="w-full divide-y divide-y-reverse">
+
         <div className="form-header flex justify-center md:justify-start bg-medium-gray h-1/6 w-full border rounded-t-md border-secondary-dark mt-2 mx-auto py-4 px-5">
           <BsLaptop className="inline my-auto mr-2" size="24" />
           <p className="text-base my-auto font-medium">New Item - Computers</p>
@@ -44,16 +41,17 @@ const ComputerForm = () => {
           <div className="first-form w-full grid md:grid-cols-2 md:mx-4">
             <div className=" mx-4 input-name my-4">
               <p className="text-sm mb-2">Name</p>
-              <input onChange={handleChange}  id="name" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
+              <input onChange={handleChange} id="name" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
             <div className=" mx-4 input-locations my-4">
               <p className="text-sm mb-2">Locations</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
-                <div className="h-10 border border-secondary-dark flex items-center">
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
+                <LocationsSubFormFE99>
+                  {/* <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
-                </div>
-                <LocationsSubFormFE99></LocationsSubFormFE99>
+                </div> */}
+                </LocationsSubFormFE99>
                 <div className="h-10 border border-secondary-dark flex items-center hover:bg-primary-light hover:text-white">
                   <TbPlus className="mx-2" />
                 </div>
@@ -67,7 +65,7 @@ const ComputerForm = () => {
                 Technician in Charge of the Hardware
               </p>
               <div className="flex divide-x divide-x-reverse">
-                <SelectComponent  onChange={handleChangeSelect} id='computermodels' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='computermodels' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border rounded-r-md border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -76,7 +74,7 @@ const ComputerForm = () => {
             <div className=" mx-4 input-group-in-charge my-4">
               <p className="text-sm mb-2">Group in Charge of the Hardware</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='computertypes' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='computertypes' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -87,16 +85,16 @@ const ComputerForm = () => {
             </div>
             <div className=" mx-4 input-alternate-username-number my-4">
               <p className="text-sm mb-2">Alternate Username Number</p>
-              <input onChange={handleChange}  id="contact_number" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
+              <input onChange={handleChange} id="contact_number" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
             <div className=" mx-4 input-alternate-username my-4">
               <p className="text-sm mb-2">Alternate Username</p>
-              <input onChange={handleChange}  id="contact" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
+              <input onChange={handleChange} id="contact" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
             <div className=" mx-4 input-user my-4">
               <p className="text-sm mb-2">User</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border rounded-r-md border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -105,7 +103,7 @@ const ComputerForm = () => {
             <div className=" mx-4 input-groups my-4">
               <p className="text-sm mb-2">Groups</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -124,7 +122,7 @@ const ComputerForm = () => {
             <div className=" mx-4 input-status my-4">
               <p className="text-sm mb-2">Status</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -136,7 +134,7 @@ const ComputerForm = () => {
             <div className=" mx-4 input-types my-4">
               <p className="text-sm mb-2">Types</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -148,7 +146,7 @@ const ComputerForm = () => {
             <div className=" mx-4 input-manufacturers my-4">
               <p className="text-sm mb-2">Manufacturers</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -160,7 +158,7 @@ const ComputerForm = () => {
             <div className=" mx-4 input-model my-4">
               <p className="text-sm mb-2">Model</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -171,16 +169,16 @@ const ComputerForm = () => {
             </div>
             <div className=" mx-4 input-serial-number my-4">
               <p className="text-sm mb-2">Serial Number</p>
-              <input onChange={handleChange}  id="serial" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
+              <input onChange={handleChange} id="serial" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
             <div className=" mx-4 input-inventory-asset-number my-4">
               <p className="text-sm mb-2">Inventory/Asset Number</p>
-              <input onChange={handleChange}  id="otherserial" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
+              <input onChange={handleChange} id="otherserial" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
             <div className=" mx-4 input-network my-4">
               <p className="text-sm mb-2">Network</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border rounded-r-md border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
@@ -188,12 +186,12 @@ const ComputerForm = () => {
             </div>
             <div className=" mx-4 input-uuid my-4">
               <p className="text-sm mb-2">UUID</p>
-              <input onChange={handleChange}  id="uuid" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
+              <input onChange={handleChange} id="uuid" className="w-full px-2 rounded-md h-10 border-1 border-secondary-dark bg-medium-gray"></input>
             </div>
             <div className=" mx-4 input-update-sources my-4">
               <p className="text-sm mb-2">Update Sources</p>
               <div className="flex divide-x divide-x-reverse">
-              <SelectComponent  onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray"/>
+                <SelectComponent onChange={handleChangeSelect} id='locations' className="w-full rounded-l-md h- border border-secondary-dark bg-medium-gray" />
                 <div className="h-10 border border-secondary-dark flex items-center">
                   <TbList className="mx-2" />
                 </div>
--- src/component/subforms/LocationsSubForm-FE99.jsx
+++ src/component/subforms/LocationsSubForm-FE99.jsx
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import { TbPlus, TbList, TbMap } from "react-icons/tb";
 
 function LocationsSubFormFE99() {
   const [open, setOpen] = useState(false);
@@ -10,14 +11,19 @@ function LocationsSubFormFE99() {
   const handleClose = () => setOpen(false);
 
   const botonAbrir = () => {
-    return <div onClick={handleOpen} className="h-10 w-10 bg-primary-light">RRR</div>
+    return (
+      //  <div onClick={handleOpen} className="h-10 w-10 bg-primary-light">RRR</div>
+      <div onClick={handleOpen} className="h-10 border border-secondary-dark flex items-center">
+        <TbList className="mx-2" />
+      </div>
+    )
   };
 
   return (
     <div>
       {botonAbrir()}
       <Modal open={open} onClose={handleClose}>
-        <div className="w-400 h-96 bg-purple-500">
+        <div className="absolute p-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-purple-500">
           <p>hooooola</p>
           <span>Si ves esto, es una prueba nomas, despues lo pongo bonito :D </span>
         </div>
