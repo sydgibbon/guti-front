37ebf3bcf2d93693b15044256aa101d7028e5061
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2023-01-01 22:44:21 -0300

    back to design login

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -41,33 +41,33 @@ const Login = () => {
   }
 
   return (
-    <div className='w-full flex flex-col justify-center items-center h-screen login-container'>
-      <div className='w-full h-1/4 flex justify-center items-center logo-section'>
+    <div className='flex flex-col w-full items-center login-container h-screen'>
+      <div className='logo-section mt-16'>
         <img className='mx-auto' src={logo} alt="guti-logo" width='200px' />
       </div>
-      <div className='form-section bg-white rounded-md h-auto w-5/6 md:w-4/5 lg:w-3/5 border border-secondary-light p-2 md:p-6'>
-        <div className='login-form flex flex-col items-center justify-center h-full w-full text-center'>
-          <div className='account-login border-b border-secondary-dark w-2/3 my-4 text-center'>
-            <span className='text-2xl font-semibold'>Account Login</span>
-          </div>
-          <form className='w-full' onSubmit={handleLoguin}>
-            <div className='user-input w-2/3 mx-auto mt-4'>
+      <div className='form-section bg-white rounded-md w-3/5 border border-secondary-light flex flex-col items-center my-8'>
+        <div className='account-login mt-20 pb-4 border-b border-secondary-dark w-1/3 text-center text-2xl font-semibold'>
+          <span className=''>Account Login</span>
+        </div>
+        <div className='login-form items-center mx-auto w-full text-center'>
+          <form onSubmit={handleLoguin}>
+            <div className='user-input w-1/3 mx-auto mt-8'>
               <p className='text-left mb-1'>Username</p>
               <input ref={username} type="text" name="username" id="username" className='w-full h-10 pl-2.5 bg-secondary-light border border-secondary-dark rounded-md' />
             </div>
-            <div className='password-input w-2/3 mx-auto mt-4'>
+            <div className='password-input w-1/3 mx-auto mt-6'>
               <p className='text-left mb-1'>Password</p>
               <input ref={password} type="password" name="password" id="password" className='w-full h-10 pl-2.5 bg-secondary-light border border-secondary-dark rounded-md' />
             </div>
-            <div className='remember-me w-2/3 text-left mx-auto mt-4'>
+            <div className='remember-me w-1/3 text-left mx-auto mt-9'>
               <input type="checkbox" name="remember-me" id="remember-me" className='mr-2.5 checkbox h-4 w-4' />
               <span className='mb-2'>Remember me</span>
             </div>
-            <button type='submit' className='block login-button text-white w-2/3 rounded-md bg-primary-light mx-auto mb-12 mt-4 h-10 font-semibold border border-orange-dark'>Login</button>
+            <button type='submit' className='block login-button w-1/3 rounded-md bg-primary-light mx-auto mb-28 mt-9 h-10 font-semibold border border-orange-dark'>Login</button>
           </form>
         </div>
       </div>
-      <div className='w-full h-1/4 m-auto flex justify-center items-center'>
+      <div className='footer text-center mb-16'>
         <p>
           GUTI Copyright (C) 2022
         </p>
