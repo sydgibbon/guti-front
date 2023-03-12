4f72b7cd81cde8447d8d559a184b00a3c015b707
ChristianGigena <nahuel.gigena@hotmail.com>
2023-03-12 17:39:10 -0300

    FIX: Dependency in Hooks

--- src/migrate/infrastructure/view/Computers/Computer.tsx
+++ src/migrate/infrastructure/view/Computers/Computer.tsx
@@ -16,6 +16,7 @@ export default function Computers() {
 
   useEffect(() => {
     dispatch(setModalIsOpen(true));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const secondaryButtonHandle = () => {
