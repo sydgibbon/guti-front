32777c3d02a80f2c6b54718645f5c9b6de13223f
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2023-03-13 19:14:50 -0300

    FIX: Software InterfazDTO

--- src/migrate/domain/dto/SoftwareDTO.ts
+++ src/migrate/domain/dto/SoftwareDTO.ts
@@ -1,26 +1,26 @@
 export interface SoftwareDTO {
       id: number;
-      entities?: any;
-      locations?: any;
-      users_tech?: any;
-      groups_tech?: any;
-      manufacturers?: any;
-      users?: any;
-      groups?: any;
-      softwarecategories?: any;
-      is_recursive: number;
-      name?: any;
-      comment?: any;
-      is_update: number;
-      is_deleted: number;
-      is_template: number;
-      template_name?: any;
-      date_mod?: any;
-      ticket_tco?: any;
-      is_helpdesk_visible: number;
-      is_valid: number;
-      date_creation?: any;
-      pictures?: any;
-      softwares?: any;
+      entities?: number;
+      locations?: number;
+      users_tech?: number;
+      groups_tech?: number;
+      manufacturers?: number;
+      users?: number;
+      groups?: number;
+      softwarecategories?: boolean;
+      is_recursive: boolean;
+      name?: string;
+      comment?: string;
+      is_update: boolean;
+      is_deleted: boolean;
+      is_template: boolean;
+      template_name?: boolean;
+      date_mod?: Date;
+      ticket_tco?: number;
+      is_helpdesk_visible: boolean;
+      is_valid: boolean;
+      date_creation?: Date;
+      pictures?: string;
+      softwares?: number;
 }
 
