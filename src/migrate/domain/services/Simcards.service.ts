1a21826c17993d4e129b1815b86eb3a37cce719d
sydgibbon <57300925+sydgibbon@users.noreply.github.com>
2023-03-19 17:02:14 -0300

    Little Fixes

--- src/migrate/domain/services/Simcards.service.ts
+++ src/migrate/domain/services/Simcards.service.ts
@@ -1,4 +1,4 @@
-import { simcardsRepository } from "../repositories/simcards.repository";
+import { simcardsRepository } from "../repositories/Simcards.repository";
 
 export const simcardsService = {
   getAllSimcards: () => {
