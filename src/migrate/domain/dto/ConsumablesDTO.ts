17e6d71e7742efee8c3e1c6fbbed0dfaebbd529a
GusChoque <110579994+GusChoque@users.noreply.github.com>
2023-03-19 18:25:30 -0300

    added null option for dto

--- src/migrate/domain/dto/ConsumablesDTO.ts
+++ src/migrate/domain/dto/ConsumablesDTO.ts
@@ -7,22 +7,22 @@ import { UsersTech } from "../models/UsersTech";
 
 
 export interface ConsumablesDTO {
-  id: number;
-  name: string;
-  ref: string;
-  is_deleted: boolean;
-  comment: string;
-  otherserial: string;
-  pictures: string;
-  entities: Entities;
-  locations: Locations;
-  consumableitemtypes: Consumableitemtypes;
-  manufacturers: Manufacturers;
-  users_tech: UsersTech;
-  groups_tech: Grounps;
-  stock_target: number;
-  is_recursive: boolean;
-  alarm_threshold: number;
-  date_mod: string;
-  date_creation: string;
+  id?: number;
+  name?: string;
+  ref?: string;
+  is_deleted?: boolean;
+  comment?: string;
+  otherserial?: string;
+  pictures?: string;
+  entities?: Entities;
+  locations?: Locations;
+  consumableitemtypes?: Consumableitemtypes;
+  manufacturers?: Manufacturers;
+  users_tech?: UsersTech;
+  groups_tech?: Grounps;
+  stock_target?: number;
+  is_recursive?: boolean;
+  alarm_threshold?: number;
+  date_mod?: string;
+  date_creation?: string;
 }
