e587c2835fd5c2356046d9461f0c49111408c679
ChristianGigena <nahuel.gigena@hotmail.com>
2023-03-22 00:49:13 -0300

    FIX: Adapter en repository

--- src/migrate/domain/repositories/phones.repository.ts
+++ src/migrate/domain/repositories/phones.repository.ts
@@ -1,10 +1,19 @@
+import { PhoneAdapter } from "../adapter/PhoneAdapter";
 import { PhoneDTO } from "../dto/PhoneDTO";
 import http from "../http/http";
 
 export const phonesRepository = {
-  getAllPhones: async () => {
+  getAllPhones: async (): Promise<PhoneAdapter[]> => {
     const response = await http.get<PhoneDTO[]>("phones/");
 
-    return response.data;
+    return response.data.map((phone) => ({
+      name: phone.name,
+      states: phone.states.name,
+      manufacturers: phone.manufacturers.name,
+      locations: phone.locations.name,
+      phonetypes: phone.phonetypes.name,
+      phonemodels: phone.phonemodels.name,
+      last_inventory_update: phone.last_inventory_update,
+    }));
   },
 };
--- src/migrate/domain/services/Phones.service.ts
+++ src/migrate/domain/services/Phones.service.ts
@@ -3,16 +3,6 @@ import { phonesRepository } from "../repositories/phones.repository";
 
 export const phonesService = {
   getAllphones: async (): Promise<PhoneAdapter[]> => {
-    const phones = await phonesRepository.getAllPhones();
-
-    return phones.map((phone) => ({
-      name: phone.name,
-      states: phone.states.name,
-      manufacturers: phone.manufacturers.name,
-      locations: phone.locations.name,
-      phonetypes: phone.phonetypes.name,
-      phonemodels: phone.phonemodels.name,
-      last_inventory_update: phone.last_inventory_update,
-    }));
+    return await phonesRepository.getAllPhones();
   },
 };
