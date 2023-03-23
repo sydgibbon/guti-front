8b8a8683c9f87e236da9e82eed970375b66e9012
ChristianGigena <nahuel.gigena@hotmail.com>
2023-03-23 20:32:03 -0300

    FIX: Import simcards service

--- src/migrate/domain/services/Simcards.service.ts
+++ src/migrate/domain/services/Simcards.service.ts
@@ -1,3 +1,5 @@
+import { simcardsRepository } from "../repositories/simcards.repository";
+
 export const simcardsService = {
   getAllSimcards: () => {
     return simcardsRepository.getAllSimcards();
