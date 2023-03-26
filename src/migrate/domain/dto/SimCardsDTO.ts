51d41f7b50cb5f22853d3f8a153243b22d785f31
FranchJavier <franciscojavierlieg@gmail.com>
2023-03-26 18:34:29 -0300

    fixed simcards table

--- src/migrate/domain/dto/SimCardsDTO.ts
+++ src/migrate/domain/dto/SimCardsDTO.ts
@@ -4,15 +4,15 @@ import { Manufacturers } from "../models/Manufacturers";
 
 export interface SimCardsDTO {
   id: number;
-  entities: Entities;
-  manufacturers: Manufacturers;
-  devicesimcardtypes: DeviceSimCardsTypes;
-  designation: string;
-  comment: string;
-  is_recursive: number;
-  is_deleted: number;
-  voltage: number;
-  date_mod: string;
-  date_creation: string;
-  allow_voip: number;
+  entities?: Entities;
+  manufacturers?: Manufacturers;
+  devicesimcardtypes?: DeviceSimCardsTypes;
+  designation?: string;
+  comment?: string;
+  is_recursive?: number;
+  is_deleted?: number;
+  voltage?: number;
+  date_mod?: string;
+  date_creation?: string;
+  allow_voip?: number;
 }
--- src/migrate/infrastructure/components/dashboard/Tables/Simcards/types.ts
+++ src/migrate/infrastructure/components/dashboard/Tables/Simcards/types.ts
@@ -1,13 +1,3 @@
 export interface DataRow {
-  designation: string;
-  comment: string;
-  entities: number;
-  manufacturers: number;
-  devicesimcardtypes: number;
-  is_recursive: number;
-  is_deleted: number;
-  voltage: number;
-  allow_voip: number;
-  date_mod: string;
-  date_creation: string;
+  id: number
 }
