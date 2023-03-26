639e9c689aebf9c0a447495bf5d7b245cc1b4b9b
GusChoque <110579994+GusChoque@users.noreply.github.com>
2023-03-26 19:29:04 -0300

    network view import fixed

--- src/migrate/domain/adapter/NetworkDevices.ts
+++
@@ -1,9 +0,0 @@
-// export interface NetworkDevice {
-//   name: string;
-//   states: number;
-//   manufacturers: string;
-//   locations: string;
-//   networkequipmenttypes: number;
-//   serial: string;
-//   date_mod: string;
-// }
