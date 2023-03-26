da7bf95a9bd6a46ca08dae903008691432ba6fa0
FranchJavier <franciscojavierlieg@gmail.com>
2023-03-26 19:56:30 -0300

    fixed devices table

---
+++ src/migrate/infrastructure/components/dashboard/Tables/Devices/types.ts
@@ -0,0 +1,10 @@
+export interface DataRow {
+name: string;
+status: string;
+manufacturers: { name: string };
+locations: { name: string };
+devicestype: { name: string };
+devicesmodel: { name: string};
+date_mod: string;
+alternateusername: string
+}
