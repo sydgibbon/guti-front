fa6efd19f2a37b30be05d3e82c152b4be2496525
GusChoque <110579994+GusChoque@users.noreply.github.com>
2023-04-16 18:08:22 -0300

    implementation getComputers

--- src/migrate/infrastructure/components/dashboard/Tables/Computers/types.ts
+++ src/migrate/infrastructure/components/dashboard/Tables/Computers/types.ts
@@ -1,10 +1,10 @@
 export interface DataRow {
   name: string;
-  states: { name: string };
-  manufacturers: { name: string };
+  states: string;
+  manufacturers: string;
   serial: string;
-  computertypes: { name: string };
-  computermodels: { name: string };
-  locations: { name: string} ;
+  computertypes: string;
+  computermodels: string;
+  locations: string ;
   date_mod: string;
 }
