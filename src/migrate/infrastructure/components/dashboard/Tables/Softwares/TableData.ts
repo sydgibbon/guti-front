20e5a353cba27a1e690ce0cef117e06b9c005175
GusChoque <110579994+GusChoque@users.noreply.github.com>
2023-04-16 18:52:53 -0300

    implemented getSoftwares method

--- src/migrate/infrastructure/components/dashboard/Tables/Softwares/TableData.ts
+++ src/migrate/infrastructure/components/dashboard/Tables/Softwares/TableData.ts
@@ -10,26 +10,26 @@ export const columns: TableColumn<DataRow>[] = [
   {
     name: "PUBLISHER",
     sortable: true,
-    selector: (row) => row.manufacturers?.name,
+    selector: (row) => row.manufacturers,
   },
   {
     name: "VERSIONS - NAME",
     sortable: true,
-    selector: (row) => row.manufacturers?.name, //hay que trabajar la logica de versions
+    selector: (row) => row.softwareversions,
   },
   {
     name: "VERSION - OPERATING SYSTEMS",
     sortable: true,
-    selector: (row) => row.manufacturers?.name, //hay que trabajar la logica de versions
+    selector: (row) => row.operatingsystems,
   },
   {
     name: "NUMBER OF INSTALLATIONS",
     sortable: true,
-    selector: (row) => row.manufacturers?.name, //hay que trabajar la logica de versions
+    selector: (row) => row.installations,
   },
   {
     name: "LICENSES - NUMBER OF LICESNSES",
     sortable: true,
-    selector: (row) => row.manufacturers?.name, //hay que trabajar la logica de licenses
+    selector: (row) => row.softwarelicenses,
   },
 ];
