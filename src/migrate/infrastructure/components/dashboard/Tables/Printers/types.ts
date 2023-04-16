f25ad52e90d8c4e103d5d7ad47b0d77e9e720490
FranchJavier <franciscojavierlieg@gmail.com>
2023-04-16 16:48:30 -0300

    implementation table printers with getPrinters

--- src/migrate/infrastructure/components/dashboard/Tables/Printers/types.ts
+++ src/migrate/infrastructure/components/dashboard/Tables/Printers/types.ts
@@ -1,10 +1,10 @@
 export interface DataRow {
   id: number;
-  locations: { name: string };
-  printertypes: { name: string };
-  manufacturers: { name: string };
-  states: { name: string};
+  locations: string;
+  printertypes: string;
+  manufacturers: string;
+  states: string;
   name: string;
   date_mod: string;
-  printermodels: { name: string};
+  printermodels: string;
 }
