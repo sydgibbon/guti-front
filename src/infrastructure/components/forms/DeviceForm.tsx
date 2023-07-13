49f0f831649860606439ebf1a62d26a636178038
FranchJavier <franciscojavierlieg@gmail.com>
2023-07-13 19:48:41 -0300

    Update "Device Form"

--- src/infrastructure/components/forms/DeviceForm.tsx
+++ src/infrastructure/components/forms/DeviceForm.tsx
@@ -16,7 +16,7 @@ import Form from "./Form";
 import { useGetDevicemodelsSelect } from "../../hooks/Devices/useGetDevicemodelsSelect";
 import { useGetDevicetypesSelect } from "../../hooks/Devices/useGetDevicetypesSelect";
 
-export default function  DeviceForm() {
+export default function DeviceForm() {
   // const computer = useCreateComputer();
 
   const handleSubmit = (e: React.SyntheticEvent) => {
@@ -30,7 +30,7 @@ export default function  DeviceForm() {
   // }, [computer.error]);
 
   const userInChargeOptions = useGetUserInChargeSelect();
-  const usersOptions = useGetUsersSelect();  
+  const usersOptions = useGetUsersSelect();
   const groupInChargeOptions = useGetGroupInChargeSelect();
   const groupsOptions = useGetGroupsSelect();
   const locationOptions = useGetLocationsSelect();
@@ -42,10 +42,12 @@ export default function  DeviceForm() {
   const deviceModelOptions = useGetDevicemodelsSelect();
   const deviceTypeOptions = useGetDevicetypesSelect();
 
-  const managementTypeOptions = [{id:"0", name:"Unit Management"},{id:"1", name:"Global Management"}]
-  
+  const managementTypeOptions = [
+    { id: "0", name: "Unit Management" },
+    { id: "1", name: "Global Management" },
+  ];
+
   useEffect(() => {
-    
     usersOptions.get();
     userInChargeOptions.get();
     groupsOptions.get();
@@ -58,83 +60,167 @@ export default function  DeviceForm() {
     autoupdatesystemOptions.get();
     deviceModelOptions.get();
     deviceTypeOptions.get();
-  
-  }, [])
+  }, []);
 
   return (
     <div className="m-6 bg-white rounded container_form_computer">
       <Form handleSubmit={handleSubmit}>
-        <TextInput
-          id={"testing"}
-          label={"Name"}
-          placeholder={"ingrese su nombre"}
-        />
 
-        <SelectOption id={"status"} label={"Status"} 
-          options={stateOptions.data?.data} />
-        <SelectOption id={"location"} label={"Locations"} options={locationOptions.data?.data} />
-        <SelectOption id="type" label="Devices Types" 
-          options={deviceTypeOptions?.data}/>
-        <SelectOption id={"hardware"} label={"Technician in charge of the hardware"}        
-          options={userInChargeOptions.data?.data}
-        />
-        <SelectOption id="manufacturer" label="Manufacturers" 
-          options={manufacturerOptions.data?.data}/>
-        <SelectOption id={"group-hardware"} label={"Group in charge of the hardware"} 
-          options={groupInChargeOptions.data?.data}
-        />
-        <SelectOption id="model" label="Model" 
-          options={deviceModelOptions?.data}/>
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Name</label>
+          <TextInput
+            id={"testing"}
+            placeholder={"ingrese su nombre"}
+          />
+        </div>
 
-        <TextInput
-          id={"alternativeusernamenumber"}
-          label="Alternate Username Number"
-          placeholder="Enter your Alternate Username number here"
-          required
-        />
-        <TextInput
-          id={"serialnumber"}
-          label="Serial Number"
-          placeholder="Enter your Serial Number here"
-          required
-        />
-        <TextInput
-          id={"alternativeusername"}
-          label="Alternate Username"
-          placeholder="Enter your Alternate Username here"
-          required
-        />
-        <TextInput
-          id={"otherserial"}
-          label="Inventory Number"
-          placeholder="Enter your Inventory Number here"
-          required
-        />
-        <SelectOption id="user" label="User"
-          options={usersOptions.data?.data} />
-        <SelectOption id="managmenttype" label="Managment Type" 
-          options={managementTypeOptions} />
-        <SelectOption id="group" label="Groups" 
-          options={groupsOptions.data?.data}
-         />
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Status</label>
+          <SelectOption
+            id={"status"}
+            options={stateOptions.data?.data}
+          />
+        </div>
 
-        <TextInput
-          id="uuid"
-          label="UUID"
-          placeholder="Enter your UUID here"
-          required
-        />
-        <TextArea
-          id={"comment"}
-          label="Comments"
-          rows={3}
-        />
-        <TextInput
-          id={"brand"}
-          label={"Brand"}
-        />
-        <SelectOption id="updatesource" label="Update Source" 
-          options={autoupdatesystemOptions.data?.data}/>
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Locations</label>
+          <SelectOption
+            id={"location"}
+            options={locationOptions.data?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Devices Types</label>
+          <SelectOption
+            id="type"
+            options={deviceTypeOptions?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Technician in charge of the hardware</label>
+          <SelectOption
+            id={"hardware"}
+            options={userInChargeOptions.data?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Manufacturers</label>
+          <SelectOption
+            id="manufacturer"
+            options={manufacturerOptions.data?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Group in charge of the hardware</label>
+          <SelectOption
+            id={"group-hardware"}
+            options={groupInChargeOptions.data?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Model</label>
+          <SelectOption
+            id="model"
+            options={deviceModelOptions?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Alternate Username Number</label>
+          <TextInput
+            id={"alternativeusernamenumber"}
+            placeholder="Enter your Alternate Username number here"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Serial Number</label>
+          <TextInput
+            id={"serialnumber"}
+            placeholder="Enter your Serial Number here"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Alternate Username</label>
+          <TextInput
+            id={"alternativeusername"}
+            placeholder="Enter your Alternate Username here"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Inventory Number</label>
+          <TextInput
+            id={"otherserial"}
+            placeholder="Enter your Inventory Number here"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">User</label>
+          <SelectOption
+            id="user"
+            options={usersOptions.data?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Managment Type</label>
+          <SelectOption
+            id="managmenttype"
+            options={managementTypeOptions}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Groups</label>
+          <SelectOption
+            id="group"
+            options={groupsOptions.data?.data}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">UUID</label>
+          <TextInput
+            id="uuid"
+            placeholder="Enter your UUID here"
+            required
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Comments</label>
+          <TextArea
+            id={"comment"}
+            rows={3}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Brand</label>
+          <TextInput
+            id={"brand"}
+          />
+        </div>
+
+        <div>
+          <label className="text-sm mb-2 font-semibold block">Update Source</label>
+          <SelectOption
+            id="updatesource"
+            options={autoupdatesystemOptions.data?.data}
+          />
+        </div>
       </Form>
     </div>
   );
