90d43c8b234d98fa63a6e11273520a0beaa3a2a5
Centeno Jose Antonio <jocecito182@gmail.com>
2023-08-04 06:18:18 -0300

    Merge branch 'sprint44' into FE143

--- src/infrastructure/components/forms/PhoneForm.tsx
+++ src/infrastructure/components/forms/PhoneForm.tsx
@@ -1,188 +1,366 @@
-import { useEffect, useState } from "react";
-import SelectOption from "../SelectOption";
-import TextArea from "../TextArea";
-import TextInput from "../TextInput";
-import Form from "./Form";
-import { useGetUserInChargeSelect } from "../../hooks/Users/useGetUserInChargeSelect";
-import { useGetUsersSelect } from "../../hooks/Users/useGetUsersSelect";
-import { useGetGroupInChargeSelect } from "../../hooks/Groups/useGetGroupInChargeSelect";
-import { useGetGroupsSelect } from "../../hooks/Groups/useGetGroupsSelect";
-import { useGetLocationsSelect } from "../../hooks/Locations/useGetLocationsSelect";
-import { useGetStatesSelect } from "../../hooks/States/useGetStatesSelect";
-import { useGetManufacturersSelect } from "../../hooks/Manufacturers/useGetManufacturersSelect";
-import { useGetAutoupdatesystemsSelect } from "../../hooks/Autoupdatesystems/useGetAutoupdatesystemsSelect";
-import { useGetPhonetypesSelect } from "../../hooks/Phones/useGetPhonetypesSelect";
-import { useGetPhonemodelsSelect } from "../../hooks/Phones/useGetPhonemodelsSelect";
-import { useGetPhonePowerSuppliesSelect } from "../../hooks/Phones/useGetPhonePowerSuppliesSelect";
-import Checkbox from "../CheckBox";
-
+import { useEffect, useState } from 'react'
+import SelectOption from '../SelectOption'
+import TextArea from '../TextArea'
+import TextInput from '../TextInput'
+import Form from './Form'
+import { useGetUserInChargeSelect } from '../../hooks/Users/useGetUserInChargeSelect'
+import { useGetUsersSelect } from '../../hooks/Users/useGetUsersSelect'
+import { useGetGroupInChargeSelect } from '../../hooks/Groups/useGetGroupInChargeSelect'
+import { useGetGroupsSelect } from '../../hooks/Groups/useGetGroupsSelect'
+import { useGetLocationsSelect } from '../../hooks/Locations/useGetLocationsSelect'
+import { useGetStatesSelect } from '../../hooks/States/useGetStatesSelect'
+import { useGetManufacturersSelect } from '../../hooks/Manufacturers/useGetManufacturersSelect'
+import { useGetAutoupdatesystemsSelect } from '../../hooks/Autoupdatesystems/useGetAutoupdatesystemsSelect'
+import { useGetPhonetypesSelect } from '../../hooks/Phones/useGetPhonetypesSelect'
+import { useGetPhonemodelsSelect } from '../../hooks/Phones/useGetPhonemodelsSelect'
+import { useGetPhonePowerSuppliesSelect } from '../../hooks/Phones/useGetPhonePowerSuppliesSelect'
+import Checkbox from '../CheckBox'
 
 export default function PhoneForm() {
-  // const computer = useCreateComputer();
+    // const computer = useCreateComputer();
 
-  const handleSubmit = (e: React.SyntheticEvent) => {
-    e.preventDefault();
-  };
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault()
+    }
 
-  // useEffect(() => {
-  //   if (computer.error) {
-  //     alert(computer.error);
-  //   }
-  // }, [computer.error]);
+    // useEffect(() => {
+    //   if (computer.error) {
+    //     alert(computer.error);
+    //   }
+    // }, [computer.error]);
 
-  interface CheckboxState {
-    headset: boolean;
-    speaker: boolean;
+    interface CheckboxState {
+        headset: boolean
+        speaker: boolean
+    }
 
-  }
+    const [checkboxes, setCheckboxes] = useState({
+        headset: false,
+        speaker: false,
+    })
 
-  const [checkboxes, setCheckboxes] = useState({
-    headset: false,
-    speaker: false,
-  });
+    const handleCheckboxChange = (checkboxName: keyof CheckboxState) => {
+        setCheckboxes((prevCheckboxes) => ({
+            ...prevCheckboxes,
+            [checkboxName]: !prevCheckboxes[checkboxName],
+        }))
+    }
 
-  const handleCheckboxChange = (checkboxName: keyof CheckboxState) => {
-    setCheckboxes((prevCheckboxes) => ({
-      ...prevCheckboxes,
-      [checkboxName]: !prevCheckboxes[checkboxName],
-    }));
-  };
+    const userInChargeOptions = useGetUserInChargeSelect()
+    const usersOptions = useGetUsersSelect()
+    const groupInChargeOptions = useGetGroupInChargeSelect()
+    const groupsOptions = useGetGroupsSelect()
+    const locationOptions = useGetLocationsSelect()
+    const stateOptions = useGetStatesSelect()
+    const manufacturerOptions = useGetManufacturersSelect()
+    const phoneModelOptions = useGetPhonemodelsSelect()
+    const phoneTypeOptions = useGetPhonetypesSelect()
+    const phonePowerSupplyTypeOptions = useGetPhonePowerSuppliesSelect()
+    const autoupdatesystemOptions = useGetAutoupdatesystemsSelect()
 
-  const userInChargeOptions = useGetUserInChargeSelect();
-  const usersOptions = useGetUsersSelect();
-  const groupInChargeOptions = useGetGroupInChargeSelect();
-  const groupsOptions = useGetGroupsSelect();
-  const locationOptions = useGetLocationsSelect();
-  const stateOptions = useGetStatesSelect();
-  const manufacturerOptions = useGetManufacturersSelect();
-  const phoneModelOptions = useGetPhonemodelsSelect();
-  const phoneTypeOptions = useGetPhonetypesSelect();
-  const phonePowerSupplyTypeOptions = useGetPhonePowerSuppliesSelect();
-  const autoupdatesystemOptions = useGetAutoupdatesystemsSelect();
+    const managementTypeOptions = [
+        { id: '0', name: 'Unit Management' },
+        { id: '1', name: 'Global Management' },
+    ]
 
-  const managementTypeOptions = [{ id: "0", name: "Unit Management" }, { id: "1", name: "Global Management" }]
+    useEffect(() => {
+        usersOptions.get()
+        userInChargeOptions.get()
+        groupsOptions.get()
+        groupInChargeOptions.get()
+        locationOptions.get()
+        stateOptions.get()
+        manufacturerOptions.get()
+        phoneModelOptions.get()
+        phoneTypeOptions.get()
+        phonePowerSupplyTypeOptions.get()
+        autoupdatesystemOptions.get()
+    }, [])
+    return (
+        <div className='m-6 bg-white rounded container_form_computer'>
+            <Form handleSubmit={handleSubmit}>
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='testing'
+                    >
+                        Name
+                    </label>
+                    <TextInput
+                        id={'testing'}
+                        placeholder={'ingrese su nombre'}
+                    />
+                </div>
 
-  useEffect(() => {
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='status'
+                    >
+                        Status
+                    </label>
+                    <SelectOption
+                        id={'status'}
+                        options={stateOptions.data?.data}
+                    />
+                </div>
 
-    usersOptions.get();
-    userInChargeOptions.get();
-    groupsOptions.get();
-    groupInChargeOptions.get();
-    locationOptions.get();
-    stateOptions.get();
-    manufacturerOptions.get();
-    phoneModelOptions.get();
-    phoneTypeOptions.get();
-    phonePowerSupplyTypeOptions.get();
-    autoupdatesystemOptions.get();
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='location'
+                    >
+                        Locations
+                    </label>
+                    <SelectOption
+                        id={'location'}
+                        options={locationOptions.data?.data}
+                    />
+                </div>
 
-  }, [])
-  return (
-    <div className="bg-white rounded container_form_computer">
-      <Form handleSubmit={handleSubmit}>
-        <TextInput
-          id={"testing"}
-          label={"Name"}
-          placeholder={"ingrese su nombre"}
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='type'
+                    >
+                        Phone types
+                    </label>
+                    <SelectOption
+                        id='type'
+                        options={phoneTypeOptions?.data}
+                    />
+                </div>
 
-        <SelectOption id={"status"} label={"Status"}
-          options={stateOptions.data?.data} />
-        <SelectOption id={"location"} label={"Locations"} options={locationOptions.data?.data} />
-        <SelectOption id="type" label="Phone types"
-          options={phoneTypeOptions?.data} />
-        <SelectOption id={"hardware"} label={"Technician in charge of the hardware"}
-          options={userInChargeOptions.data?.data}
-        />
-        <SelectOption id="manufacturer" label="Manufacturers"
-          options={manufacturerOptions.data?.data} />
-        <SelectOption id={"group-hardware"} label={"Group in charge of the hardware"}
-          options={groupInChargeOptions.data?.data}
-        />
-        <SelectOption id="model" label="Model"
-          options={phoneModelOptions?.data} />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='hardware'
+                    >
+                        Technician in charge of the hardware
+                    </label>
+                    <SelectOption
+                        id={'hardware'}
+                        options={userInChargeOptions.data?.data}
+                    />
+                </div>
 
-        <TextInput
-          id={"alternativeusernamenumber"}
-          label="Alternate Username Number"
-          placeholder="Enter your Alternate Username number here"
-          required
-        />
-        <TextInput
-          id={"serialnumber"}
-          label="Serial Number"
-          placeholder="Enter your Serial Number here"
-          required
-        />
-        <TextInput
-          id={"alternativeusername"}
-          label="Alternate Username"
-          placeholder="Enter your Alternate Username here"
-          required
-        />
-        <TextInput
-          id={"otherserial"}
-          label="Inventory Number"
-          placeholder="Enter your Inventory Number here"
-          required
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='manufacturer'
+                    >
+                        Manufacturers
+                    </label>
+                    <SelectOption
+                        id='manufacturer'
+                        options={manufacturerOptions.data?.data}
+                    />
+                </div>
 
-        <SelectOption id="user" label="User"
-          options={usersOptions.data?.data} />
-        <SelectOption id="managmenttype" label="Managment Type"
-          options={managementTypeOptions} />
-        <SelectOption id="group" label="Groups"
-          options={groupsOptions.data?.data}
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='group-hardware'
+                    >
+                        Group in charge of the hardware
+                    </label>
+                    <SelectOption
+                        id={'group-hardware'}
+                        options={groupInChargeOptions.data?.data}
+                    />
+                </div>
 
-        <TextInput
-          id="uuid"
-          label="UUID"
-          placeholder="Enter your UUID here"
-          required
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='model'
+                    >
+                        Model
+                    </label>
+                    <SelectOption
+                        id='model'
+                        options={phoneModelOptions?.data}
+                    />
+                </div>
 
-        <TextArea
-          id={"comment"}
-          label="Comments"
-          rows={3}
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='alternativeusernamenumber'
+                    >
+                        Alternate Username Number
+                    </label>
+                    <TextInput
+                        id={'alternativeusernamenumber'}
+                        placeholder='Enter your Alternate Username number here'
+                        required
+                    />
+                </div>
 
-        <TextInput
-          id={"brand"}
-          label={"Brand"}
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='serialnumber'
+                    >
+                        Serial Number
+                    </label>
+                    <TextInput
+                        id={'serialnumber'}
+                        placeholder='Enter your Serial Number here'
+                        required
+                    />
+                </div>
 
-        <SelectOption id="updatesource" label="Update Source"
-          options={autoupdatesystemOptions.data?.data} />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='alternativeusername'
+                    >
+                        Alternate Username
+                    </label>
+                    <TextInput
+                        id={'alternativeusername'}
+                        placeholder='Enter your Alternate Username here'
+                        required
+                    />
+                </div>
 
-        <TextInput
-          id={"number_line"}
-          label="Number of Lines"
-          type={"number"}
-        />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='otherserial'
+                    >
+                        Inventory Number
+                    </label>
+                    <TextInput
+                        id={'otherserial'}
+                        placeholder='Enter your Inventory Number here'
+                        required
+                    />
+                </div>
 
-        <SelectOption id="phonepowersupplytype" label="Phone Power Supply Types"
-          options={phonePowerSupplyTypeOptions?.data} />
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='user'
+                    >
+                        User
+                    </label>
+                    <SelectOption
+                        id='user'
+                        options={usersOptions.data?.data}
+                    />
+                </div>
 
-        <div className="  rounded-lg">
-          <div className="mb-2 font-semibold">Ports</div>
-          <div className="grid grid-cols-5 gap-2">
-            <Checkbox
-              id="have_headset"
-              label="Headset"
-              checked={checkboxes.headset}
-              onChange={() => handleCheckboxChange("headset")}
-            />
-            <Checkbox
-              id="have_hp"
-              label="Speaker"
-              checked={checkboxes.speaker}
-              onChange={() => handleCheckboxChange("speaker")}
-            />
-          </div>
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='managmenttype'
+                    >
+                        Managment Type
+                    </label>
+                    <SelectOption
+                        id='managmenttype'
+                        options={managementTypeOptions}
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='group'
+                    >
+                        Groups
+                    </label>
+                    <SelectOption
+                        id='group'
+                        options={groupsOptions.data?.data}
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='uuid'
+                    >
+                        UUID
+                    </label>
+                    <TextInput
+                        id='uuid'
+                        placeholder='Enter your UUID here'
+                        required
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='comment'
+                    >
+                        Comments
+                    </label>
+                    <TextArea
+                        id={'comment'}
+                        rows={3}
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='brand'
+                    >
+                        Brand
+                    </label>
+                    <TextInput
+                        id={'brand'}
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='updatesource'
+                    >
+                        Update Source
+                    </label>
+                    <SelectOption
+                        id='updatesource'
+                        options={autoupdatesystemOptions.data?.data}
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className='text-sm mb-2 font-semibold block'
+                        htmlFor='number_line'
+                    >
+                        Number of Lines
+                    </label>
+                    <TextInput
+                        id={'number_line'}
+                        type={'number'}
+                    />
+                </div>
+
+                <div className='  rounded-lg'>
+                    <div className='mb-2 font-semibold'>Ports</div>
+                    <div className='grid grid-cols-5 gap-2'>
+                        <Checkbox
+                            id='have_headset'
+                            label='Headset'
+                            checked={checkboxes.headset}
+                            onChange={() => handleCheckboxChange('headset')}
+                        />
+                        <Checkbox
+                            id='have_hp'
+                            label='Speaker'
+                            checked={checkboxes.speaker}
+                            onChange={() => handleCheckboxChange('speaker')}
+                        />
+                    </div>
+                </div>
+            </Form>
         </div>
-      </Form>
-    </div>
-  );
+    )
 }
