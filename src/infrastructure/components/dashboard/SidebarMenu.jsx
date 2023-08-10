c6d27df999cb584939f0edb00316d2407d154019
Centeno Jose Antonio <jocecito182@gmail.com>
2023-08-10 07:50:58 -0300

    Revert "Fix EdidAsset toggle"
    This reverts commit 87dcad0a75f4b62fd28a2b957d16946202fd02ba.

--- src/infrastructure/components/dashboard/SidebarMenu.jsx
+++ src/infrastructure/components/dashboard/SidebarMenu.jsx
@@ -1,44 +1,27 @@
-import { BsChevronCompactDown } from 'react-icons/bs'
-import Item from '../atomic/Item'
+import { BsChevronCompactDown } from "react-icons/bs";
+import Item from "../atomic/Item";
 
-const SidebarMenu = ({
-    text,
-    icon,
-    subMenu,
-    selected,
-    setSelected,
-    hasOptions,
-    context,
-}) => {
-    const setSidebarMenuItemSelected = () => {
-        if (selected !== text) setSelected(text)
-    }
+const SidebarMenu = ({ text, icon, subMenu, selected, setSelected, hasOptions, context }) => {
+  const toggle = () => {selected !== text ? setSelected(text) : setSelected(false)}
+  return (
+    <>
+      <div
+        className={`py-2 px-3 flex justify-between hover:border-l-2 border-primary-light cursor-normal select-none 
+        ${context === "edditAsset" ? 
+          selected === text && "shadow-md border-l-2"
+          :
+          selected === text && !hasOptions && "border-l-2 bg-white font-bold"
 
-    return (
-        <>
-            <div
-                className={`py-2 px-3 flex justify-between hover:border-l-2 border-primary-light cursor-normal select-none 
-        ${
-            context === 'edditAsset'
-                ? selected === text && 'shadow-md border-l-2'
-                : selected === text &&
-                  !hasOptions &&
-                  'border-l-2 bg-white font-bold'
         } 
         `}
-                onClick={setSidebarMenuItemSelected}
-            >
-                <Item
-                    icon={icon}
-                    text={text}
-                />
-                {hasOptions && (
-                    <BsChevronCompactDown className='block my-auto' />
-                )}
-            </div>
-            {selected === text && subMenu}
-        </>
-    )
-}
+        onClick={toggle}
+      >
+        <Item icon={icon} text={text} />
+        {hasOptions && <BsChevronCompactDown className="block my-auto" />}
+      </div>
+      {selected === text && subMenu}
+    </>
+  );
+};
 
-export default SidebarMenu
+export default SidebarMenu;
--- src/infrastructure/view/Assets/EditAsset.tsx
+++ src/infrastructure/view/Assets/EditAsset.tsx
@@ -1,72 +1,72 @@
 import {
-    AssetOptions,
-    EditAssetOption,
-} from '../../contexts/asset/AssetOptions'
-import { useLocation } from 'react-router-dom'
-import React, { useEffect, useState } from 'react'
-import SidebarMenu from '../../components/dashboard/SidebarMenu'
-import { capitalize } from '@mui/material'
+  AssetOptions,
+  EditAssetOption,
+} from "../../contexts/asset/AssetOptions";
+import { useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import SidebarMenu from "../../components/dashboard/SidebarMenu";
+import { capitalize } from "@mui/material";
 
 export default function EditAsset() {
-    const [selectedOption, setSelectedOption] = useState<string>('')
+  const [selectedOption, setSelectedOption] = useState<string>("");
 
-    const assetPath = useLocation().pathname
-    const asset = assetPath.substring(assetPath.lastIndexOf('/') + 1)
+  const assetPath = useLocation().pathname;
+  const asset = assetPath.substring(assetPath.lastIndexOf("/") + 1);
 
-    const selectedOptions: EditAssetOption | undefined = AssetOptions.find(
-        (locName) => locName.name === capitalize(asset)
-    )
+  const selectedOptions: EditAssetOption | undefined = AssetOptions.find(
+    (locName) => locName.name === capitalize(asset)
+  );
 
-    const handleOptionClick = (optionName: string) => {
-        setSelectedOption(optionName)
+  const handleOptionClick = (optionName: string) => {
+    setSelectedOption(optionName);
+  };
+
+  // Set the initial selected option to the first item in the options list
+  useEffect(() => {
+    if (
+      selectedOption === "" &&
+      selectedOptions &&
+      selectedOptions.options.length > 0
+    ) {
+      setSelectedOption(selectedOptions.options[0].name);
     }
+  }, []);
 
-    // Set the initial selected option to the first item in the options list
-    useEffect(() => {
-        if (
-            selectedOption === '' &&
-            selectedOptions &&
-            selectedOptions.options.length > 0
-        ) {
-            setSelectedOption(selectedOptions.options[0].name)
-        }
-    }, [])
-
-    return (
-        <div className='flex flex-col edit-asset-container'>
-            <div className='flex form-sidebar-section'>
-                <div className='mt-12 w-56 sidebar'>
-                    {selectedOptions &&
-                        selectedOptions.options.map((option) => (
-                            <SidebarMenu
-                                key={option.name}
-                                text={option.name}
-                                selected={selectedOption}
-                                setSelected={handleOptionClick}
-                                icon={<></>}
-                                hasOptions={false}
-                                subMenu={<></>}
-                                context={'editAsset'}
-                            />
-                        ))}
-                </div>
-                <div className='flex flex-col w-full title-form-section'>
-                    {selectedOption && (
-                        <div className='box-border block title'>
-                            <span className='p-2 text-lg font-semibold bg-white rounded-md form-title'>
-                                {`${selectedOptions?.name} - ExampleAsset`}
-                            </span>
-                        </div>
-                    )}
-
-                    <div className='mt-5 mr-6 form'>
-                        {selectedOption &&
-                            selectedOptions?.options.find(
-                                (option) => option.name === selectedOption
-                            )?.content}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col edit-asset-container">
+      <div className="flex form-sidebar-section">
+        <div className="mt-12 w-56 sidebar">
+          {selectedOptions &&
+            selectedOptions.options.map((option) => (
+              <SidebarMenu
+                key={option.name}
+                text={option.name}
+                selected={selectedOption}
+                setSelected={handleOptionClick}
+                icon={<></>}
+                hasOptions={false}
+                subMenu={<></>}
+                context={"editAsset"}
+              />
+            ))}
         </div>
-    )
+        <div className="flex flex-col w-full title-form-section">
+          {selectedOption && (
+            <div className="box-border block title">
+              <span className="p-2 text-lg font-semibold bg-white rounded-md form-title">
+                {`${selectedOptions?.name} - ExampleAsset`}
+              </span>
+            </div>
+          )}
+
+          <div className="mt-5 mr-6 form">
+            {selectedOption &&
+              selectedOptions?.options.find(
+                (option) => option.name === selectedOption
+              )?.content}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
