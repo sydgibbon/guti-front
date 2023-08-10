154bad796cf7c542a27b6e1d4ea40854181fd75f
Centeno Jose Antonio <jocecito182@gmail.com>
2023-08-10 08:01:51 -0300

    Fix EditAsset bug

--- src/infrastructure/components/dashboard/SidebarMenu.jsx
+++ src/infrastructure/components/dashboard/SidebarMenu.jsx
@@ -1,22 +1,36 @@
 import { BsChevronCompactDown } from "react-icons/bs";
 import Item from "../atomic/Item";
 
-const SidebarMenu = ({ text, icon, subMenu, selected, setSelected, hasOptions, context }) => {
-  const toggle = () => {selected !== text ? setSelected(text) : setSelected(false)}
+const SidebarMenu = ({
+  text,
+  icon,
+  subMenu,
+  selected,
+  setSelected,
+  hasOptions,
+  context,
+}) => {
+  const setSidebarMenuItemSelected = () => {
+    if (selected !== text) setSelected(text);
+  };
   return (
     <>
       <div
         className={`py-2 px-3 flex justify-between hover:border-l-2 border-primary-light cursor-normal select-none 
-        ${context === "edditAsset" ? 
-          selected === text && "shadow-md border-l-2"
-          :
-          selected === text && !hasOptions && "border-l-2 bg-white font-bold"
-
+        ${
+          context === "edditAsset"
+            ? selected === text && "shadow-md border-l-2"
+            : selected === text &&
+              !hasOptions &&
+              "border-l-2 bg-white font-bold"
         } 
         `}
-        onClick={toggle}
+        onClick={setSidebarMenuItemSelected}
       >
-        <Item icon={icon} text={text} />
+        <Item
+          icon={icon}
+          text={text}
+        />
         {hasOptions && <BsChevronCompactDown className="block my-auto" />}
       </div>
       {selected === text && subMenu}
