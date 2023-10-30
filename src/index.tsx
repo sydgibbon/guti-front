53c6590247e70c521d5e73610f1694fc0e9e11d0
sydgibbon <57300925+sydgibbon@users.noreply.github.com>
2023-10-29 23:05:17 -0300

    import updated

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { UserContextProvider } from "./infrastructure/contexts/userContext";
-import i18n from "./i18n";
+import './i18n';
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
